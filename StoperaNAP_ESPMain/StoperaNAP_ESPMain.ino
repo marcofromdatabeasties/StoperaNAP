b88819df4af731a4012d223130075089d55be308
marcoframdatabeasties <marco@databeasties.com>
2021-02-13 17:23:19 +0100

    added defines and first program layout

---
+++ StoperaNAP_ESPMain/StoperaNAP_ESPMain.ino
@@ -0,0 +1,48 @@
+
+/**
+ * Programma dat aan de hand van de (deels) verwachte waterstanden boven NAP in het Stopera monument de juiste waarde laat zien.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+#define PUMP 23 //pomp GPIO23, om water bij te pompen
+#define SOLENOID 24 //solenoid GPIO24, water weg te laten lopen.
+#define PRESSURE 5 // FSR op ADC1_0, meet de hoeveelheid water in kolom.
+#define MAX_PRES 4095 //max conversie waarde: 0-4095 bij 0.1-3.2 volt.
+
+#define W1_LAAG 0 // state 1: water in kolom te laag tov NAP/ water in column to low
+#define W2_HOOG 1 // state 2: water in kolom te hoog tov NAP/water in column to high
+#define W3_GOED 2 // state 3: water in kolom goed tov NAP/water in column ok
+#define W4_ERROR 3 // state 4: water in kolom veranderd niet / meting onjuist/ error state: water is not moving or measurement is off.
+
+#define C_HEIGHT 120 // 1.2 meter
+#define C_R 5 //5 cm radius
+
+#define FLOW_IN 7 //liter per minute.
+#define FLOW_OUT 5 //liter per minute.
+
+uint8_t state = W2_GOED;
+
+
+void setup() {
+  pinMode(PUMP, OUTPUT);
+  pinMode(SOLENOID, OUTPUT);
+  
+
+}
+
+void loop() {
+  vTaskDelete(NULL); //cancel the loop, we use tasks
+}
