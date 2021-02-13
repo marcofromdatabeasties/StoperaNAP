12d7c92c2e5c6195e3508b136d60e171ed660f0c
marcofromdatabeasties <marco@databeasties.com>
2021-02-13 17:49:05 +0100

    Added defines, code Wifi and Keys file

---
+++ StoperaNAP_ESPMain/Keys.h
@@ -0,0 +1,2 @@
+#define ST_SSID "somenetwork"
+#define PASSWORD "very_secret"
--- StoperaNAP_ESPMain/StoperaNAP_ESPMain.ino
+++ StoperaNAP_ESPMain/StoperaNAP_ESPMain.ino
@@ -16,9 +16,12 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
+#include <WiFi.h>
+#include "Keys.h"
 
 #define PUMP 23 //pomp GPIO23, om water bij te pompen
-#define SOLENOID 24 //solenoid GPIO24, water weg te laten lopen.
+#define SOLENOID 22 //solenoid GPIO24, water weg te laten lopen.
+#define WIFI_ON 21 //Wifi active led indicator
 #define PRESSURE 5 // FSR op ADC1_0, meet de hoeveelheid water in kolom.
 #define MAX_PRES 4095 //max conversie waarde: 0-4095 bij 0.1-3.2 volt.
 
@@ -26,6 +29,7 @@
 #define W2_HOOG 1 // state 2: water in kolom te hoog tov NAP/water in column to high
 #define W3_GOED 2 // state 3: water in kolom goed tov NAP/water in column ok
 #define W4_ERROR 3 // state 4: water in kolom veranderd niet / meting onjuist/ error state: water is not moving or measurement is off.
+#define START 4 //state 5: starting (WIFI)
 
 #define C_HEIGHT 120 // 1.2 meter
 #define C_R 5 //5 cm radius
@@ -33,12 +37,29 @@
 #define FLOW_IN 7 //liter per minute.
 #define FLOW_OUT 5 //liter per minute.
 
-uint8_t state = W2_GOED;
+uint8_t state = START;
+
+void initiateWIFIConnection(void * parameter) {
+  //start the WIFI and connect.
+  digitalWrite(WIFI_ON, LOW);
+  WiFi.begin(ST_SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+  digitalWrite(WIFI_ON, HIGH);
+  vTaskDelete(NULL); // done 
+}
 
 
 void setup() {
   pinMode(PUMP, OUTPUT);
   pinMode(SOLENOID, OUTPUT);
+  pinMode(WIFI_ON, OUTPUT);
+
+  digitalWrite(PUMP, LOW); // pump off
+  digitalWrite(SOLENOID, HIGH); // solenoid closed
+
   
 
 }
