56beabee7c56374c74188d7a7f0908e517f855f2
marcofromdatabeasties <marco@databeasties.com>
2021-02-18 15:17:52 +0100

    added first tasks and worldmodel

--- StoperaNAP_ESPMain/StoperaNAP_ESPMain.ino
+++ StoperaNAP_ESPMain/StoperaNAP_ESPMain.ino
@@ -1,8 +1,9 @@
-  
+
+
 /**
- * Programma dat aan de hand van de (deels) verwachte waterstanden boven NAP in het Stopera monument de juiste waarde laat zien.
- * 
- * This program is free software: you can redistribute it and/or modify
+   Programma dat aan de hand van de (deels) verwachte waterstanden boven NAP in het Stopera monument de juiste waarde laat zien.
+
+   This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -14,9 +15,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- */
+
+*/
 #include <WiFi.h>
+#include <dummy.h>
 #include "Keys.h"
 
 #ifndef ST_SSID
@@ -27,15 +29,17 @@
 
 #define PUMP 23 //pomp GPIO23, om water bij te pompen
 #define SOLENOID 22 //solenoid GPIO24, water weg te laten lopen.
-#define WIFI_ON 21 //Wifi active led indicator
-#define PRESSURE 5 // FSR op ADC1_0, meet de hoeveelheid water in kolom.
+#define WIFI_ON 21 //inet active led indicator
+#define ERROR_ON 19 //error led indicator
+#define PRESSURE 36 // FSR op ADC1_0, meet de hoeveelheid water in kolom.
 #define MAX_PRES 4095 //max conversie waarde: 0-4095 bij 0.1-3.2 volt.
 
-#define W1_LAAG 0 // state 1: water in kolom te laag tov NAP/ water in column to low
-#define W2_HOOG 1 // state 2: water in kolom te hoog tov NAP/water in column to high
-#define W3_GOED 2 // state 3: water in kolom goed tov NAP/water in column ok
-#define W4_ERROR 3 // state 4: water in kolom veranderd niet / meting onjuist/ error state: water is not moving or measurement is off.
-#define START 4 //state 5: starting (WIFI)
+#define W1_LAAG 0 // world 1: water in kolom te laag tov NAP/ water in column to low
+#define W2_HOOG 1 // world 2: water in kolom te hoog tov NAP/water in column to high
+#define W3_GOOD 2 // world 3: water in kolom goed tov NAP/water in column ok
+#define W4_ERROR 3 // world 4: water in kolom veranderd niet / meting onjuist/ error world: water is not moving or measurement is off.
+#define START 4 //world 5: starting (INet)
+#define NOWHERE 5 //world 6: starting (INet)
 
 #define C_HEIGHT 120 // 1.2 meter
 #define C_R 5 //5 cm radius
@@ -43,18 +47,97 @@
 #define FLOW_IN 7 //liter per minute.
 #define FLOW_OUT 5 //liter per minute.
 
-uint8_t state = START;
+uint8_t volatile world = NOWHERE; // we start from nowhere
+uint8_t volatile next_world = START;
 
-void initiateWIFIConnection(void * parameter) {
-  //start the WIFI and connect.
-  digitalWrite(WIFI_ON, LOW);
-  WiFi.begin(ST_SSID, PASSWORD);
+uint16_t volatile pressureNminus2;
+uint16_t volatile pressureNminus1;
+uint16_t volatile pressureCurrent;
 
-    while (WiFi.status() != WL_CONNECTED) {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+SemaphoreHandle_t xSemaphore_world = NULL; //mutex for worldchanges
+SemaphoreHandle_t xSemaphore_P = NULL; //mutex for new pressure values
+SemaphoreHandle_t xSemaphore_INET = NULL; //mutex for connection status
+
+bool addNewPressureValue(uint16_t pressureNew) {
+  if ( xSemaphore_P != NULL ) {
+    /* See if we can obtain the semaphore.  If the semaphore is not
+      available wait 10 ticks to see if it becomes free. */
+    if ( xSemaphoreTake( xSemaphore_P, ( TickType_t ) 30 ) == pdTRUE ) {
+      pressureNminus1 = pressureCurrent;
+      pressureNminus2 = pressureNminus1;
+      pressureCurrent = pressureNew;
+      xSemaphoreGive( xSemaphore_P );
     }
-  digitalWrite(WIFI_ON, HIGH);
-  vTaskDelete(NULL); // done 
+    return true;
+
+  } else {
+    return false; //error setup not completed.
+  }
+}
+
+void initiateINETConnection(void * parameter) {
+  //start the WIFI and connect, for now (Ethernet later)
+  digitalWrite(WIFI_ON, LOW);
+  if ( xSemaphore_INET != NULL ) {
+    if ( xSemaphoreTake( xSemaphore_INET, ( TickType_t ) 100 ) == pdTRUE ) {
+
+      WiFi.begin(ST_SSID, PASSWORD);
+
+      while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+      }
+      xSemaphoreGive( xSemaphore_INET );
+
+      digitalWrite(WIFI_ON, HIGH);
+      if ( xSemaphore_world != NULL ) {
+        if ( xSemaphoreTake( xSemaphore_world, ( TickType_t ) 100 ) == pdTRUE ) {
+          world = W3_GOOD;
+          xSemaphoreGive( xSemaphore_world );
+        }
+      }
+    }
+  }
+  vTaskDelete(NULL); // done
+}
+
+void handleWorlds(void * parameter) {
+  //handles the world changes and fires the corresponding tasks
+  for ( ;; ) {
+    if ( xSemaphore_world != NULL ) {
+      if ( xSemaphoreTake( xSemaphore_world, ( TickType_t ) 100 ) == pdTRUE ) {
+        if (world != next_world) {
+          switch (next_world) {
+            case W1_LAAG:
+
+              break;
+            case W2_HOOG:
+
+              break;
+            case W3_GOOD:
+
+              break;
+            case W4_ERROR:
+
+              break;
+            case START:
+              xTaskCreate(
+                initiateINETConnection,
+                "iNet_Connection",
+                1000,
+                NULL,
+                1,
+                NULL            // Task handle
+              );
+
+              break;
+            default:
+              xSemaphoreGive( xSemaphore_world );
+          }
+        }
+      }
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // wait a second
+  }
 }
 
 
@@ -64,10 +147,22 @@ void setup() {
   pinMode(WIFI_ON, OUTPUT);
 
   digitalWrite(PUMP, LOW); // pump off
-  digitalWrite(SOLENOID, HIGH); // solenoid closed
+  digitalWrite(SOLENOID, LOW); // solenoid closed
+  digitalWrite(WIFI_ON, LOW); // INET off
+  digitalWrite(ERROR_ON, LOW); // INET off
 
-  
+  xSemaphore_world = xSemaphoreCreateMutex();
+  xSemaphore_P = xSemaphoreCreateMutex();
+  xSemaphore_INET = xSemaphoreCreateMutex();
 
+  xTaskCreate(
+    handleWorlds,
+    "Worlds",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
