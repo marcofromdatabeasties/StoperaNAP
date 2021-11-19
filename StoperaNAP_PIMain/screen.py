374fb1f3bc411fe627693dfbdfeb952c0522f858
marcofromdatabeasties <marco@databeasties.com>
2021-11-19 17:02:17 +0100

    bugs

--- StoperaNAP_PIMain/screen.py
+++ StoperaNAP_PIMain/screen.py
@@ -27,7 +27,7 @@ class LCD:
 
     def writeToScreen(self, channel, location, status, current_level , desired_level):
         self.lock.acquire(True, 10)
-        print ("desired: " + desired_level)
+        print ("desired: {}".format(desired_level))
         text = "{location} {status} {current_level:2.2f}/{desired_level:2.2f}" + (' ' * 20).format(
                 location = location, status = status, 
                 current_level=round(current_level, 2) , desired_level = round(desired_level, 2))[:20]
